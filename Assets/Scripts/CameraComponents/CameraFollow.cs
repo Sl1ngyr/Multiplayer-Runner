b7f8c699edfc9f4cf40aa4a12df25e8c353d171b
Sl1ngyr <60054609+Sl1ngyr@users.noreply.github.com>
2024-04-21 14:58:53 +0300

    implemented camera follow
    Додав камеру, яка буде слідкувати за гравцем

---
+++ Assets/Scripts/CameraComponents/CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace CameraComponents
+{
+    public class CameraFollow : MonoBehaviour
+    {
+        private Transform _cameraAnchorPoint;
+        
+        [SerializeField] private Vector3 _cameraOffest;
+        
+        public Transform CameraAnchorPoint
+        {
+            get => _cameraAnchorPoint;
+            set => _cameraAnchorPoint = value;
+        }
+        
+        private void LateUpdate()
+        {
+            if(_cameraAnchorPoint == null) return;
+
+            transform.position = _cameraAnchorPoint.position + _cameraOffest;
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerCameraHandler.cs
@@ -0,0 +1,21 @@
+﻿using CameraComponents;
+using Fusion;
+using UnityEngine;
+
+namespace Player
+{
+    public class PlayerCameraHandler : NetworkBehaviour
+    {
+        private Camera _camera;
+
+        public override void Spawned()
+        {
+            if (HasStateAuthority)
+            {
+                _camera = Camera.main;
+                
+                _camera.GetComponent<CameraFollow>().CameraAnchorPoint = transform;
+            }
+        }
+    }
+}
