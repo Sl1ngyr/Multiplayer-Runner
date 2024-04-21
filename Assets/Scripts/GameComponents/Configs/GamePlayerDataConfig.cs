9f03d8c9b5c83bbbef3aac0a56caa2e4f2334c0d
Sl1ngyr <60054609+Sl1ngyr@users.noreply.github.com>
2024-04-21 15:04:29 +0300

    implemented players cars config
    Реалізовано компонент, який тримає дані машин, та повертає об'єкт машини за номером.

---
+++ Assets/Scripts/GameComponents/Configs/GamePlayerDataConfig.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameComponents.Configs
+{
+    public class GamePlayerDataConfig : MonoBehaviour
+    {
+        [field: SerializeField] public List<PlayerCarData> PlayersCars { get; private set; }
+
+        public GameObject GetPlayerCarByID(int ID)
+        {
+            GameObject playerCar = null;
+            
+            foreach (var car in PlayersCars)
+            {
+                if(car.CarID == ID) playerCar = car.PlayerCar;
+            }
+
+            if(playerCar == null) Debug.LogError("There is no car with this ID");
+            
+            return playerCar;
+        }
+    }
+}
