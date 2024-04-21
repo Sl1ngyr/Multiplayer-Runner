d99803eceff090811171aaaf20eb0581cff53432
Sl1ngyr <60054609+Sl1ngyr@users.noreply.github.com>
2024-04-21 14:59:40 +0300

    implemented network player spawner
    Реалізовано спавн гравців

---
+++ Assets/Scripts/Services/Network/PlayerSpawner.cs
@@ -0,0 +1,37 @@
+﻿using Fusion;
+using GameComponents.Configs;
+using Services.Const;
+using UnityEngine;
+
+namespace Services.Network
+{
+    public class PlayerSpawner : SimulationBehaviour, IPlayerJoined
+    {
+        [SerializeField] private GamePlayerDataConfig _playerDataConfig;
+
+        private int _networkCarId;
+        
+        public void PlayerJoined(PlayerRef player)
+        {
+            if (Runner.LocalPlayer == player)
+            {
+                _networkCarId = PlayerPrefs.GetInt(Constants.PLAYER_PREFS_CAR_ID);
+                
+                if (Runner.LocalPlayer.PlayerId == 1)
+                {
+                    Runner.Spawn(_playerDataConfig.GetPlayerCarByID(_networkCarId), 
+                        new Vector3(-Constants.RUNNER_OFFSET_TO_SPAWN_PLAYER, 0,0), 
+                        Quaternion.identity);
+                }
+                else
+                {
+                    Runner.Spawn(_playerDataConfig.GetPlayerCarByID(_networkCarId), 
+                        new Vector3(Constants.RUNNER_OFFSET_TO_SPAWN_PLAYER, 0,0), 
+                        Quaternion.identity);
+                }
+                
+            }
+            
+        }
+    }
+}
